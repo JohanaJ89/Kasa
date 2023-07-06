ce44507602aabfd4bbac629e4d86098e71a7f528
JohanaJ89 <jupinjohana@gmail.com>
2023-07-06 23:13:57 +0200

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+const Navigation = () => {
+    return (
+        <div className='navigation'>
+            <ul>
+                <NavLink to="/">
+                    <li>
+                        Accueil
+                    </li>
+                </NavLink>
+                <NavLink to="/About">
+                    <li>
+                        A propos
+                    </li>
+                </NavLink>
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
